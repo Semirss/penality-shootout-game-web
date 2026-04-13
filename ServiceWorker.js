b7863e55612dc908d01105698a2733a0d4c4308a
Semir_S <149691609+Semirss@users.noreply.github.com>
2026-04-13 09:26:52 +0300

    first commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Gomida-penaliity shootout game-1.0";
+const contentToCache = [
+    "Build/penality shootout game web.loader.js",
+    "Build/penality shootout game web.framework.js.unityweb",
+    "Build/penality shootout game web.data.unityweb",
+    "Build/penality shootout game web.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
